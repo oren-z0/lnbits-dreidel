5638d5430de4721180f08618e4860bf66dd79126
Oren <orenz0@protonmail.com>
2024-12-20 15:02:54 +0200

    dreidel end and withdraw apis

--- views_api.py
+++ views_api.py
@@ -1,13 +1,16 @@
 from asyncio import Queue
 from http import HTTPStatus
 import json
-from typing import Optional
-from urllib import request
 import random
 from time import time
-from fastapi import Depends, Query, Request, Response
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+from fastapi import Depends, Query, Request
 from fastapi.exceptions import HTTPException
 from loguru import logger
+from lnurl import encode as lnurl_encode
+
+from lnbits.helpers import urlsafe_short_hash
+from lnbits.core.services import pay_invoice
 
 try:
     import Cryptodome.Random.random
@@ -184,3 +187,112 @@ async def _get_amount_paid(dreidel: Dreidel) -> int: # in millisats
 
         return payment.amount
     return 0
+
+@dreidel_ext.post("/api/v1/dreidels/{dreidel_id}/end")
+async def api_dreidel_end(req: Request, dreidel_id: str):
+    dreidel = await get_dreidel(dreidel_id)
+    if not dreidel:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="Dreidel game instance does not exist."
+        )
+    game_state = json.loads(dreidel.game_state)
+    if game_state["state"] == "ended":
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail="Dreidel game already ended."
+        )
+    game_state["state"] = "ended"
+    leftover_base = game_state["jackpot"] // dreidel.players
+    leftover_remainder = game_state["jackpot"] % dreidel.players
+    game_state["balances"] = [
+        balance + leftover_base + (1 if player_index < leftover_remainder else 0)
+        for player_index, balance in enumerate(game_state["balances"])
+    ]
+    game_state["jackpot"] = 0
+    game_state["locked"] = False
+    game_state["withdraw_links"] = [
+        _build_withdraw_link(req, dreidel.id, player_index, balance)
+        for player_index, balance in enumerate(game_state["balances"])
+    ]
+    game_state["updated_at"] = time()
+    await update_dreidel_game_state(dreidel, dreidel.wallet, game_state, "")
+    return {"ok": True}
+
+def _build_withdraw_link(req: Request, dreidel_id: str, player_index: int, balance: int) -> dict:
+    k1 = urlsafe_b64encode(json.dumps([dreidel_id, player_index, urlsafe_short_hash()]))
+    # pay_invoice upper limit is defined by max_sat, so we can't accept millisats.
+    # They will be left in the server's wallet.
+    amount_sats = balance // 1000
+    if amount_sats <= 0:
+        return {
+            "status": "too_small"
+        }
+    return {
+        "amount_sats": balance // 1000,
+        "k1": k1,
+        "status": "pending",
+        "lnurl": lnurl_encode(req.url_for("api_dreidels_withdraw", k1=k1)),
+    }
+
+@dreidel_ext.get("/api/v1/dreidels-withdraw")
+async def api_dreidels_withdraw(req: Request, k1: str, pr: str | None = None):
+    try:
+        data = json.loads(urlsafe_b64decode(k1))
+        if not isinstance(data, list) or len(data) != 3 or not isinstance(data[0], str) or not isinstance(data[1], int) or not isinstance(data[2], str):
+            return {"status": "ERROR", "reason": "Invalid k1."}
+        dreidel_id = data[0]
+        player_index = data[1]
+        dreidel = await get_dreidel(dreidel_id)
+        if not dreidel:
+            return {"status": "ERROR", "reason": "Invalid k1."}
+        game_state = json.loads(dreidel.game_state)
+        if game_state["state"] != "ended":
+            return {"status": "ERROR", "reason": "Invalid k1."}
+        if player_index < 0 or player_index >= len(game_state["withdraw_links"]):
+            return {"status": "ERROR", "reason": "Invalid k1."}
+        withdraw_link = game_state["withdraw_links"][player_index]
+        if withdraw_link["status"] == "too_small":
+            return {"status": "ERROR", "reason": "Invalid k1."}
+        if withdraw_link["k1"] != k1:
+            return {"status": "ERROR", "reason": "Invalid k1."}
+        if withdraw_link["status"] != "pending":
+            return {"status": "ERROR", "reason": "Already withdrawn."}
+        amount_sats = withdraw_link["amount_sats"]
+        if not pr:
+            return {
+                "tag": "withdrawRequest",
+                "callback": req.url_for("api_dreidels_withdraw"), # same endpoint
+                "k1": k1,
+                "maxWithdrawable": amount_sats * 1000,
+                "minWithdrawable": amount_sats * 1000,
+                "defaultDescription": f"Dreidel game prize {dreidel_id} player {player_index + 1}",
+            }
+        if game_state["locked"]:
+            return {"status": "ERROR", "reason": "Withdraw already being processed."}
+        game_state["locked"] = True
+        try:
+            await update_dreidel_game_state(dreidel, dreidel.wallet, game_state, "")
+            dreidel = await get_dreidel(dreidel_id)
+            if not dreidel:
+                return {"status": "ERROR", "reason": "Dreidel game instance does not exist anymore."}
+            game_state = json.loads(dreidel.game_state)
+        except:
+            return {"status": "ERROR", "reason": "Withdraw already being processed."}
+        try:
+            await pay_invoice(
+                wallet_id=dreidel.wallet,
+                payment_request=pr,
+                max_sat=amount_sats,
+                extra={"tag": "dreidel-withdraw", "id": dreidel_id, "player_index": player_index},
+            )
+            withdraw_link["status"] = "paid"
+        except:
+            return {"status": "ERROR", "reason": "Failed to pay invoice."}
+        finally:
+            game_state["locked"] = False
+            try:
+                await update_dreidel_game_state(dreidel, dreidel.wallet, game_state, "")
+            except:
+                return {"status": "ERROR", "reason": "Critical error in unlocking withdraw-links state."}
+        return {"status": "OK"}
+    except:
+        return {"status": "ERROR", "reason": "Internal error."}
